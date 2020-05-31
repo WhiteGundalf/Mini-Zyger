bfea563b75be207faaaa260126b6d233b188722c
daneel olivaw <34041307+therealdaneel@users.noreply.github.com>
2020-05-31 16:41:18 +0200

    Help Command Implementation
    Removed the ugly default help command and replaced it with a pretty embedded one.

--- bot.py
+++ bot.py
@@ -1,21 +1,27 @@
 import discord
 from discord.ext import commands
 
-token = 'TOKEN'
+token = 'NzAzMTk2MDQwMTQ0MzU1MzYx.XtO6uA.ho14tqyP_36KIgB996r57ebH1HU'
+
+def get_prefix(client, message):
+    prefixes = ['>']  # sets the prefixes, u can keep it as an array of only 1 item if you need only one prefix
+
+    if not message.guild:
+        prefixes = ['>']  # Only allow '==' as a prefix when in DMs, this is optional
+    return commands.when_mentioned_or(*prefixes)(client, message)
 
 #client = commands.AutoShardedBot(self_bot=False, command_prefix=">", case_insensitive=True, shard_count=2)
-client = commands.Bot(command_prefix = '>', case_insensitive = True)
+client = commands.Bot(command_prefix = get_prefix, case_insensitive = True)
 cogs = ['cogs.basic']
 
 @client.event
 async def on_ready():
-    print('connected and online')
+	print("connected and online")
+	client.remove_command('help')
+	for cog in cogs:
+		try:
+			client.load_extension(cog)
+		except Exception as error:
+			print(error)
 
-if __name__ == '__main__':
-    for cog in cogs:
-        try:
-            client.load_extension(cog)
-        except Exception as error:
-            print(f'cog {cog} failed to load. Error: {error}')
-
-    client.run(token)
+client.run(token, bot=True, reconnect=True)
