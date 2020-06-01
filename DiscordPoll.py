5c12f3beb7bbe525d306ed7329da818bacd94139
daneel olivaw <34041307+therealdaneel@users.noreply.github.com>
2020-06-01 22:05:31 +0200

    Poll cogs now shows results, Fixed help bug
    Listener Cog was causing issues with the help command due to it not having any commands.
    poll cog now fetches and displays results.

--- DiscordPoll.py
+++ DiscordPoll.py
@@ -15,7 +15,7 @@ class Polls(commands.Cog):
 
     @commands.command(name="poll", description="creates a poll", pass_context=True, usage="title; amount of options; option1; option2; option3(etc.)(max = 10); ExpirationTime(defaults to 12h)(formatted like [2h 32m]) ")
     @commands.cooldown(1, 60, BucketType.member)
-    @has_permissions(administrator=True)
+    #@has_permissions(manage_messages=True)
     async def poll(self, ctx):
         if not ctx.message.author.bot:
             try:
@@ -26,6 +26,8 @@ class Polls(commands.Cog):
                     if i != ">poll" and i != "<@!716640483471917176> poll":
                         title += i + ' '
                 AmountOfOptions = int(MsgContentList[1])
+                if AmountOfOptions > 10:
+                    AmountOfOptions = 10
                 Options = MsgContentList[2:]
                 Options = Options[:AmountOfOptions]
                 Expiration = MsgContentList[-1]
@@ -56,22 +58,27 @@ class Polls(commands.Cog):
                 except:
                     await ctx.channel.send("invalid expiration time, did you format it like '[hours]h [minutes]m'? [ex. 2h 30m]")
                     return
-                #await ctx.message.delete()
+                await ctx.message.delete()
                 if TotalTime == 0:
                     TotalTime = 12 * 60 * 60
                     hours = 12
                     minutes = 0
-                PollEmbed = discord.Embed(title=title, color=0x9B59B6)
+                PollEmbed = discord.Embed(title=f"__{title}__", color=0x9B59B6)
+                PollEmbed.set_author(name=f"requested by {ctx.message.author.name}", icon_url=ctx.message.author.avatar_url)
                 PollEmbed.set_thumbnail(url=self.client.user.avatar_url)
                 OptionCounter= 0
                 Emotes = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+                EmoteDict = {Emotes[0]:":one:", Emotes[1]:":two:", Emotes[2]:":three:", Emotes[3]:":four:", Emotes[4]:":five:", Emotes[5]:":six:", Emotes[6]:":seven:", Emotes[7]:":eight:", Emotes[8]:":nine:", Emotes[9]:":keycap_ten:"}
                 for i in Options:
                     OptionCounter += 1
-                    PollEmbed.add_field(name="\u200b", value=f"`{OptionCounter}:` {i}", inline=False)
+                    PollEmbed.add_field(name="\u200b", value=f"**{OptionCounter}**: {i}", inline=False)
                 PollEmbed.set_footer(text=f"expires in {hours}h {minutes}m")
                 message = await ctx.channel.send(embed=PollEmbed)
+                MESSAGE_ID=message.id
+                AddedReactions = []
                 for i in range(OptionCounter):
                     await message.add_reaction(Emotes[i])
+                    AddedReactions.append(Emotes[i])
                 TimeMessageCreated = time.time()
                 timeleft = TotalTime
                 while round(time.time() - TimeMessageCreated) < TotalTime:
@@ -92,9 +99,43 @@ class Polls(commands.Cog):
                         ReformatMinutes = round(workingTime/60)
                     PollEmbed.set_footer(text=f"Expires in {ReFormatHours}h {ReformatMinutes}m")
                     await message.edit(embed=PollEmbed)
-                await ctx.channel.send(f"<@!{ctx.message.author.id}>, the poll has finished.")
+                poll = await ctx.channel.fetch_message(MESSAGE_ID)
+                Results_unrefined = {}
+                Results = {}
+                RefiningResultCounter = 0
+                for i in poll.reactions:
+                    if RefiningResultCounter < 3:
+                        Results.update({EmoteDict[i.emoji]:i.count-1})
+                    RefiningResultCounter += 1
+                TotalScore = 0
+                for i in [int(Results[i]) for i in list(Results.keys())]:
+                    TotalScore += i
+                EmbedResults = {}
+
+                for i in Results.keys():
+                    try:
+                        EmbedResults.update({i:[Results[i], round(Results[i]/TotalScore*100, 2)]})
+                    except DivisionByZero as e:
+                        EmbedResults.update({i:[Results[i], 0]})
+                        print(e)
+
+                FinalPollEmbed = discord.Embed(title=title+"[POLL OVER]", color=0x9B59B6)
+                PollEmbed.set_author(name=f"requested by {ctx.message.author.name}", icon_url=ctx.message.author.avatar_url)
+                FinalPollEmbed.set_thumbnail(url=self.client.user.avatar_url)
+                OptionsToEmotes = {0:":one:", 1:":two:", 2:":three:", 3:":four:", 4:":five:", 5:":six:", 6:":seven:", 7:":eight:", 8:":nine:", 9:":keycap_ten:"}
+                OptionCounter= 0
+                for i in Options:
+                    try:
+                        FinalPollEmbed.add_field(name="\u200b", value=f"`{OptionCounter}:` {i}\t**{EmbedResults[OptionsToEmotes[OptionCounter]][1]}%** with **{EmbedResults[OptionsToEmotes[OptionCounter]][0]}** votes", inline=False)
+                        OptionCounter += 1
+                    except Exception as e:
+                        print(e)
+                FinalPollEmbed.set_footer(text="expired")
+                await message.edit(embed=FinalPollEmbed)
+                    
 
             except Exception as e:
+                print(e)
                 await ctx.channel.send("Error, did you use the correct format? (>help Polls for more info)")
     @poll.error
     async def poll_error(obj, ctx, error):
@@ -108,9 +149,11 @@ class Polls(commands.Cog):
                 else:
                     ToSend += ', ' + str(i)
             if len(error.missing_perms) == 1:
-                await ctx.channel.send(f"You are missing the following perm: **{ToSend}**")
+                await ctx.channel.send(f"You are missing the following perm: **{ToSend}**", delete_after=3)
+                await ctx.message.delete()
             else:
-                await ctx.channel.send(f"You are missing the following perms: **{ToSend}**")
+                await ctx.channel.send(f"You are missing the following perms: **{ToSend}**", delete_after=3)
+                await ctx.message.delete()
 
 
 
