6c2cbc75a820fce0aad7979bccb4c06020c9c3dc
daneel olivaw <34041307+therealdaneel@users.noreply.github.com>
2020-06-02 09:59:52 +0200

    Removed Token

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-token = 'NzE2NjQwNDgzNDcxOTE3MTc2.XtPCfw.rx_xDnwTIgoVxQUIsriKj6SBTRw'
+token = 'Token'
 
 def get_prefix(client, message):
     prefixes = ['>']  # sets the prefixes, u can keep it as an array of only 1 item if you need only one prefix
